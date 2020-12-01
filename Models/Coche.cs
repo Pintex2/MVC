20728c817e95866ad2c402d8ab7fbc1c7d80bd4e
Andre Pinto <andrepinto@Andres-MacBook-Pro.local>
2020-12-01 10:48:24 -0500

    first commit

---
+++ Models/Coche.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Project.Models
+{
+    public class Coche
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public Coche()
+        {
+        }
+    }
+}
